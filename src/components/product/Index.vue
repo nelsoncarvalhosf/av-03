e5493c44151f4f33aea81298d1a0b2c986a9eed0
exododigitalmkt <exododigitalmkt@gmail.com>
2021-05-12 11:28:04 -0300

    ex-av-03

---
+++ src/components/product/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="container">
+    <img class="brand" alt="Vue logo" width="80px" src="@/assets/logo.png">
+    <h2 class="text-right">Shopping Cart: {{this.$store.state.products.length}}</h2>
+    <b-container class="col-md-3">
+      <div><ProductForm /></div>
+      <div class>
+        <router-link to="products">listagem de produtos</router-link>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import ProductForm from "./ProductForm";
+
+
+export default {
+  name: "productApp",
+  components: {
+    ProductForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  margin-top: 40px;
+}
+.qtd-produtos-container {
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/product/ProductsList.vue
@@ -0,0 +1,38 @@
+<template>  
+  <b-container>
+    <div style="text-align:left; margin-bottom:15px; font-size:40px">
+        <router-link to="/">Voltar</router-link>
+    </div>
+    <b-table-simple>
+      <b-thead>
+        <b-th>Produto</b-th>
+        <b-th>Qtde</b-th>
+        <b-th>Valor unitário</b-th>
+        <b-th>Valor Total do item</b-th>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="product in this.$store.state.products" v-bind:key="product.name" cols="5">
+          <ProductItem :product="product" />
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
+  </b-container>
+</template>
+
+<script>
+import ProductItem from "./ProductItem";
+
+export default {
+  name: "ProductsList",
+  components: {
+    ProductItem,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
